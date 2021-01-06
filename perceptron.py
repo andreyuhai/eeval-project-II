849f1ff6272db6089f495d26e49d3ba139020a48
andreyuhai <burakaymakci@gmail.com>
2021-01-06 20:09:01 +0100

    Implemented functions to animate the training of a perceptrong, such as draw_weight_vector(), draw_decision_boundary() ...

--- perceptron.py
+++ perceptron.py
@@ -3,20 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# config.frame_width = 30
-
-# class VectorArrow(Scene):
-#    def construct(self):
-#       print(config.frame_height)
-#       dot = Dot(ORIGIN)
-#       arrow = Arrow(ORIGIN, [2, 2, 0], buff=0)
-#       numberplane = NumberPlane(y_max=7, y_min=-7)
-#       origin_text = Text('(0, 0)').next_to(dot, DOWN)
-#       tip_text = Text('(2, 2)').next_to(arrow.get_end(), RIGHT)
-
-#       self.play(ShowCreation(numberplane))
-#       self.add(numberplane, dot, arrow, origin_text, tip_text)
-#       self.wait(5)
+config.frame_width = 30
 
 # y = np.array([[1],
 #          [1],
@@ -31,82 +18,149 @@ import numpy as np
 #          [2, -1],
 #          [3, -2]])
 
+
+# positive_samples, negative_samples
+
+class Perceptron(Scene):
+    # def __init__(self):
+    #     self.weight_vector = None
+    #     self.position_vector = None
+    #     self.decision_boundary = None
+    #     self.weights = np.zeros((3, 1))
+    #     self.bias = 0
+
+    def construct(self):
+        self.weight_vector = None
+        self.position_vector = None
+        self.decision_boundary = None
+        self.weights_text = None
+        self.weights = np.zeros((3, 1))
+        self.bias = 0
+        # self.weights = np.zeros((3, 1))
+        # self.bias = 0
+        r = np.max(np.linalg.norm(np.vstack((self.x_pos, self.x_neg)), axis=1))
+        learning_rate = 0.1
+
+        self.numberplane = NumberPlane(y_min=-10, y_max=10)
+        positive_dots = VGroup(*[Dot(point=p) for p in self.x_pos])
+        negative_dots = VGroup(*[Dot(point=p) for p in self.x_neg])
+
+        positive_dots.set_color(WHITE)
+        negative_dots.set_color(RED)
+
+        self.play(ShowCreation(self.numberplane))
+        self.play(ShowCreation(positive_dots))
+        self.play(ShowCreation(negative_dots))
+        self.wait(1)
+
+        self.show_weights()
+        self.draw_weight_vector()
+        self.draw_decision_boundary()
+
+        while True:
+            for idx, sample in enumerate(np.vstack((self.x_pos, self.x_neg))):
+                self.draw_position_vector(sample)
+                if np.sign(np.dot(sample, self.weights) - self.bias) != np.vstack((self.y_pos, self.y_neg))[idx]:
+                    self.weights += np.vstack((self.y_pos, self.y_neg))[idx] * learning_rate * sample.reshape(len(sample), 1)
+                    self.bias -= learning_rate * np.vstack((self.y_pos, self.y_neg))[idx] * r ** 2
+
+                    self.show_weights()
+                    print(f"Weights: {self.weights}\nBias: {self.bias}")
+                    self.draw_weight_vector()
+                    if idx == 0:
+                        print("first decision boundary call")
+                    self.draw_decision_boundary()
+                    if idx == 0:
+                        print("done decision boundary call")
+
+            if np.all(np.sign(np.dot(np.vstack((self.x_pos, self.x_neg)), self.weights) - self.bias) == np.vstack((self.y_pos,
+                self.y_neg))):
+                break
+
+
+    def draw_weight_vector(self):
+        if self.weight_vector:
+            # TODO: add offset to ORIGIN
+            self.play(Transform(self.weight_vector, Arrow(ORIGIN, self.weights.T[0], buff=0)))
+            self.wait(0.5)
+        else:
+            if np.any(self.weights):
+                self.weight_vector = Arrow(ORIGIN, self.weights.T[0], buff=0)
+                self.play(ShowCreation(self.weight_vector))
+                self.wait(0.5)
+
+    def draw_decision_boundary(self):
+        f = lambda x : float(-(self.weights[0] / self.weights[1]) * x + self.bias / self.weights[1])
+
+        if self.decision_boundary:
+            print("if self decision boundary girdi")
+            self.play(Transform(self.decision_boundary, Line([self.numberplane.x_min,  f(self.numberplane.x_min), 0],
+                [self.numberplane.x_max, f(self.numberplane.x_max), 0])))
+            self.wait(0.5)
+        else:
+            if np.any(self.weights):
+                print("np.any girdi")
+                self.decision_boundary = Line([self.numberplane.x_min,  f(self.numberplane.x_min), 0],
+                        [self.numberplane.x_max, f(self.numberplane.x_max), 0])
+                print('created')
+                self.play(ShowCreation(self.decision_boundary))
+                self.wait(1)
+            else:
+                print("burada")
+
+    def draw_position_vector(self, point):
+        if self.position_vector:
+            self.play(FadeOut(self.position_vector))
+            self.wait(0.5)
+            # TODO: Add offset to Arrow ORIGIN
+            self.play(Transform(self.position_vector, Arrow(ORIGIN, point, buff=0)))
+            self.wait(0.5)
+        else:
+            # TODO: Add offset to Arrow ORIGIN
+            self.position_vector = Arrow(ORIGIN, point, buff=0)
+            self.play(ShowCreation(self.position_vector))
+            self.wait(0.5)
+
+    def show_weights(self):
+        if self.weights_text:
+            self.play(Transform(self.weights_text, Text(f"w: ({float(self.weights[0])}, {float(self.weights[1])})\nb: {self.bias}").to_corner(UL)))
+        else:
+            self.weights_text = Text(f"w: ({float(self.weights[0])}, {float(self.weights[1])})\nb: {self.bias}").to_corner(UL)
+            self.play(ShowCreation(self.weights_text))
+
 # Create the matrix
-N = 20
-M = np.zeros((N, 2))
+N = 6
+M = np.zeros((N, 3))
 
 np.random.seed(317054)
 
-M[:,0] = np.random.normal(size=(20,))
-M[0:int(N/2), 1] = np.random.uniform(size=(10,)) + 0.5
-M[int(N/2):, 1] = -np.random.uniform(size=(10, )) - 0.5
+M[:,0] = np.random.normal(size=(N,))
+M[0:N//2, 1] = np.random.uniform(size=(N//2,)) + 0.5
+M[N//2:, 1] = -np.random.uniform(size=(N//2, )) - 0.5
 angle = np.random.normal()
 
 x = np.zeros(M.shape)
 x[:, 0] = M[:, 0] * np.cos(angle) - M[:, 1] * np.sin(angle)
 x[:, 1] = M[:, 0] * np.sin(angle) + M[:, 1] * np.cos(angle)
 
-x += np.random.normal(size=(1,2))
+x[:, 0:2] += np.random.normal(size=(1,2))
 y = np.sign(M[:, 1].reshape(len(M[:, 1]), 1))
 
-# positive_samples, negative_samples
+x = np.array([[1, 2, 0],
+    [3, 3, 0],
+    [1.5, -2, 0],
+    [-1, -2, 0],
+    [-2, -1, 0],
+    [-3, -1.5, 0]])
 
-class Foo(Scene):
-    def construct(self):
-        self.weights = np.zeros((3, 1))
-        self.bias = 0
-        r = np.max(np.linalg.norm(np.vstack(self.x_pos, self.x_neg), axis=1))
-        learning_rate = 0.1
+y = np.ones((6,1))
+y[3:, :] *= -1
 
-        # numberplane = NumberPlane(y_min=-10, y_max=10)
-        numberplane = NumberPlane()
-        # positive_dots = VGroup(*[Dot(point=x * RIGHT + y * UP) for x, y in x[:int(N/2), :]])
-        # negative_dots = VGroup(*[Dot(point=x * RIGHT + y * UP) for x, y in x[int(N/2):, :]])
-        positive_dots = VGroup(*[Dot(point=x) for point in x[:int(N/2), :]])
+perceptron = Perceptron()
+perceptron.x_pos = x[0:N//2, :]
+perceptron.y_pos = y[0:N//2, :]
 
-        positive_dots.set_color(WHITE)
-        negative_dots.set_color(RED)
+perceptron.x_neg = x[N//2:, :]
+perceptron.y_neg = y[N//2:, :]
 
-        self.play(ShowCreation(numberplane))
-        self.play(ShowCreation(positive_dots))
-        self.play(ShowCreation(negative_dots))
-        # self.wait(2)
-
-        self.draw_weight_vector(self.weights)
-        while True:
-            for idx, sample in enumerate(positive_dots):
-                self.play(ShowCreation(Arrow(ORIGIN, sample.get_center(), buff=0)))
-
-                # if np.sign(np.dot(sample.get_center(), self.weights) - self.bias) != y[idx]:
-
-
-            # for idx, sample in enumerate(negative_dots):
-
-
-
-        # w = np.array([1, -2])
-        # bias = 1
-        # eta = 0.1
-        # r = np.max(np.linalg.norm(x, axis=1))
-        self.draw_decision_boundary(self.weights, self.bias, numberplane.x_min, numberplane.x_max)
-
-        self.play(ShowCreation(Arrow(ORIGIN, positive_dots[0].get_center(), buff=0)))
-        self.wait(5)
-
-
-    def draw_weight_vector(self, weights):
-        self.weight_vector = Arrow(ORIGIN, weights[0] * RIGHT + weights[1] * UP, buff=0)
-        self.play(ShowCreation(self.weight_vector))
-
-    def update_weight_vector(self, new_weights):
-        pass
-
-    def draw_decision_boundary(self, weights, bias, x_min, x_max):
-        f = lambda x : -(weights[0] / weights[1]) * x + bias / weights[1]
-
-        self.decision_boundary = Line([ x_min,  f(x_min), 0 ], [ x_max, f(x_max), 0 ])
-        self.play(ShowCreation(self.decision_boundary))
-        self.wait(1)
-
-    def draw_position_vector(self, point, offset):
-        pass
+perceptron.render()
